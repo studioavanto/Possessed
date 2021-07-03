7653d974e95f8e4310a647cd36a93635c20b2f22
Ilmosal <ilmosal@gmail.com>
2021-07-03 18:42:00 +0300

    Added spikes

---
+++ Scripts/Objects/Spikes.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+export var spikes_on = true
+
+func toggle_spikes():
+	spikes_on = not spikes_on
+	
+	if spikes_on:
+		$HurtBox.set_collision_layer_bit(2, true)
+	else:
+		$HurtBox.set_collision_layer_bit(2, false)
