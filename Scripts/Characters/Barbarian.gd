b8d33d25016dfbaadbe49c4a7e043ac5c89b0f30
Ville Jantunen <ville.e.jantunen@gmail.com>
2021-07-03 19:29:40 +0300

    barbarian can carry things

--- Scripts/Characters/Barbarian.gd
+++ Scripts/Characters/Barbarian.gd
@@ -1,31 +1,40 @@
 extends "res://Scripts/Characters/PossessCharacter.gd"
 
-export var carry_point_offset = Vector2(0,5.0)
+export var carry_point_offset = Vector2(0,-64.0)
 
 var carry_item = null
-var carry_point = self.position + carry_point_offset
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CarryHitBox.disabled = true
 
 func carry_nearby():
 	for area in $CarryingArea.get_overlapping_areas():
 		start_carrying_target(area.get_parent())
 		if carry_item != null:
+			carry_item.is_being_carried = false
 			break
 
 func start_carrying_target(target):
 	if target.carry_target():
 		carry_item = target
-		set_collision_mask_bit(0, false)
+		$CarryHitBox.disabled = false
+		set_collision_mask_bit(3, false)
 
 func stop_carrying():
-	set_collision_mask_bit(0, true)
+	set_collision_mask_bit(3, true)
+	$CarryHitBox.disabled = true
+	carry_item.is_being_carried = true
 	carry_item = null
 
 func process_special():
 	carry_nearby()
+
+func _physics_process(delta):
+	._physics_process(delta)
+	
+	if carry_item != null:
+		carry_item.position = position + carry_point_offset
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
