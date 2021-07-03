6bd85ab0b8b5dd60cb29f3803ce24f7460764612
Havoq <jonas.haverinen@helsinki.fi>
2021-07-03 22:48:44 +0300

    removed collision from PossessCharacter and Thief

--- Scripts/Characters/Thief.gd
+++ Scripts/Characters/Thief.gd
@@ -1,15 +1,17 @@
-extends Node2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+extends "res://Scripts/Characters/PossessCharacter.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func process_special():
+	pass
+	
+func get_id():
+	return 2
+
+func _physics_process(_delta):
+	._physics_process(_delta)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
