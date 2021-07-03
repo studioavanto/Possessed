b2af134a423b3764652dde677d378870c6c44f64
Ilmosal <ilmosal@gmail.com>
2021-07-03 23:43:09 +0300

    Added more animation stuff and fixed platforms

--- Scripts/Objects/PlatformMoving.gd
+++ Scripts/Objects/PlatformMoving.gd
@@ -3,6 +3,7 @@ extends Line2D
 export var platform_speed = 40
 var torward_point = 1
 var wait = false
+var stop = false
 
 func _ready():
 	$Timer.wait_time = 1.0
@@ -12,8 +13,11 @@ func begin_movement():
 	torward_point = (torward_point + 1) % 2
 	wait = false
 
+func toggle_platform():
+	stop = not stop
+
 func _physics_process(delta):
-	if wait:
+	if wait or stop:
 		return
 
 	$Platform.position -= ($Platform.position - get_point_position(torward_point)).normalized() * delta * platform_speed
