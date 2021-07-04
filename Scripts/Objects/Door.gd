7d9ae35a71bb31de0ebe4a1f4aa3b1d8b58441b5
Ville Jantunen <ville.e.jantunen@gmail.com>
2021-07-04 23:30:24 +0300

    Button interactions with stuff now works properly and also added a bunch of animations

--- Scripts/Objects/Door.gd
+++ Scripts/Objects/Door.gd
@@ -4,12 +4,13 @@ export var door_on = true
 
 func toggle_door():
 	door_on = not door_on
-	door_on = not door_on
 	
 	if door_on:
 		set_collision_layer_bit(0, true)
+		$AnimatedSprite.animation = "closing"
 	else:
 		set_collision_layer_bit(0, false)
+		$AnimatedSprite.animation = "opening"
 
 func _on_Button_toggle_off():
 	toggle_door() 
--- Scripts/Objects/Spikes.gd
+++ Scripts/Objects/Spikes.gd
@@ -6,8 +6,10 @@ func toggle_spikes():
 	spikes_on = not spikes_on
 	
 	if spikes_on:
+		animation = "ouchy"
 		$HurtBox.set_collision_layer_bit(2, true)
 	else:
+		animation = "retract"
 		$HurtBox.set_collision_layer_bit(2, false)
 
 func _on_Button_toggle_off():
