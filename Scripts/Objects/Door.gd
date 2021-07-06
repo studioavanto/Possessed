41d205a6f1a29d583042c512bf04cd487a3e0d86
Ilmosal <ilmosal@gmail.com>
2021-07-06 17:29:25 +0300

    Fixed door bug when closing doors when there are boxes or character in the way

--- Scripts/Objects/Door.gd
+++ Scripts/Objects/Door.gd
@@ -3,6 +3,9 @@ extends StaticBody2D
 export var door_on = true
 
 func toggle_door():
+	for area in $CheckCollisions.get_overlapping_areas():
+		return
+
 	door_on = not door_on
 	
 	if door_on:
@@ -16,4 +19,10 @@ func _on_Button_toggle_off():
 	toggle_door() 
 
 func _on_Button_toggle_on():
+	toggle_door()
+
+func _on_Switch_toggle_on():
+	toggle_door()
+
+func _on_Switch_toggle_off():
 	toggle_door()
