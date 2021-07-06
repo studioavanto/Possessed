6e1a9816a3839d0e7d6cc7d0c17ea1748172b9c4
Jonas Haverinen <jonas.haverinen@helsinki.fi>
2021-07-06 19:45:46 +0300

    added level transition monologue

--- Scripts/Control/CutsceneController.gd
+++ Scripts/Control/CutsceneController.gd
@@ -3,5 +3,5 @@ extends "res://Scripts/Control/Controllable.gd"
 signal proceed
 
 func handle_inputs():
-	if Input.is_action_just_pressed("special"):
+	if Input.is_action_just_pressed("special") or Input.is_action_just_pressed("interact"):
 		emit_signal("proceed")
--- Scripts/UI/Monologue.gd
+++ Scripts/UI/Monologue.gd
@@ -1,15 +1,13 @@
-extends TextureRect
+extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$RichTextLabel.set_bbcode("")
-	$RichTextLabel.set_visible_characters(0)
+	self.set_bbcode("This is all test stuff")
+	self.set_visible_characters(0)
 	$Timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
 func _on_Timer_timeout():
-	$RichTextLabel.set_visible_characters($RichTextLabel.get_visible_characters()+1)
+	self.set_visible_characters(self.get_visible_characters()+1)
 	
-	if $RichTextLabel.get_visible_characters() > $RichTextLabel.get_total_character_count():
+	if self.get_visible_characters() > self.get_total_character_count():
 		$Timer.stop()
