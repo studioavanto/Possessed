9b656b4498a987db56d3f63f3c4b074d76294a30
Jonas Haverinen <jonas.haverinen@helsinki.fi>
2021-07-06 21:11:11 +0300

    adjusted monologue; added initial dictionaries for monologues and level titles

--- Scripts/UI/Monologue.gd
+++ Scripts/UI/Monologue.gd
@@ -2,7 +2,7 @@ extends RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.set_bbcode("This is all test stuff")
+	self.set_bbcode("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 	self.set_visible_characters(0)
 	$Timer.start()
 
