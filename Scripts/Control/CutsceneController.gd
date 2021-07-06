7e8d516e6f07c5b1b55b57a232fdab449dcf8544
Jonas Haverinen <jonas.haverinen@helsinki.fi>
2021-07-06 21:36:03 +0300

    adjusted text box; added cutscene controller options; removed interact key from cutscene controller

--- Scripts/Control/CutsceneController.gd
+++ Scripts/Control/CutsceneController.gd
@@ -3,5 +3,7 @@ extends "res://Scripts/Control/Controllable.gd"
 signal proceed
 
 func handle_inputs():
-	if Input.is_action_just_pressed("special") or Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("special") \
+	or Input.is_action_just_pressed("press_up") \
+	or Input.is_action_just_pressed("get_older"):
 		emit_signal("proceed")
