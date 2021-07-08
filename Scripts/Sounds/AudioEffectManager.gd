56d28020ecd8a7699f1f3591f4a67679f1be8c6a
Ilmosal <ilmosal@gmail.com>
2021-07-08 12:30:54 +0300

    Fixed file paths for new sounds

--- Scripts/Sounds/AudioEffectManager.gd
+++ Scripts/Sounds/AudioEffectManager.gd
@@ -1,13 +1,13 @@
 extends Node
 
 onready var audio_effects = {
-	"ansa_piikki": [ preload("res://Resources/Sounds/Objects/ansapiikki.wav"), 0.0 ],
-	"boski_raahaus": [ preload("res://Resources/Sounds/Objects/boksiraahaus.wav"), 0.0 ],
-	"boksi_rikki": [ preload("res://Resources/Sounds/Objects/boksirikki.wav"), 0.0 ],
-	"kalteri_ovi": [ preload("res://Resources/Sounds/Objects/kalteriovi.wav"), 0.0 ],
-	"lattia_nappi": [ preload("res://Resources/Sounds/Objects/Lattianappi.wav"), 0.0 ],
-	"object_fall": [ preload("res://Resources/Sounds/Objects/objectfall.wav"), 0.0 ],
-	"vipu": [ preload("res://Resources/Sounds/Objects/vipu.wav"), 0.0 ],
+	"ansa_piikki": [ preload("res://Resources/Sounds/Objects/ansapiikki.ogg"), 0.0 ],
+	"boski_raahaus": [ preload("res://Resources/Sounds/Objects/boksiraahaus.ogg"), 0.0 ],
+	"boksi_rikki": [ preload("res://Resources/Sounds/Objects/boksirikki.ogg"), 0.0 ],
+	"kalteri_ovi": [ preload("res://Resources/Sounds/Objects/kalteriovi.ogg"), 0.0 ],
+	"lattia_nappi": [ preload("res://Resources/Sounds/Objects/lattianappi.ogg"), 0.0 ],
+	"object_fall": [ preload("res://Resources/Sounds/Objects/objectfall.ogg"), 0.0 ],
+	"vipu": [ preload("res://Resources/Sounds/Objects/vipu.ogg"), 0.0 ],
 	"game_over": [ preload("res://Resources/Sounds/UI/gameover.ogg"), 0.0 ],
 	"level_transition": [ preload("res://Resources/Sounds/UI/leveltransition.ogg"), 0.0 ],
 	"mark_as_possessed": [ preload("res://Resources/Sounds/UI/mark_as_possessed.ogg"), 0.0 ],
@@ -15,7 +15,7 @@ onready var audio_effects = {
 	"warp_to_host": [ preload("res://Resources/Sounds/UI/warp_to_host.ogg"), 0.0 ]
 }
 
-export var silenced = true
+export var silenced = false
 
 func play_sound(sound_string):
 	if silenced:
