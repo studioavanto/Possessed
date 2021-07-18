5f6f814edb794df6c2f97d7ae285a4de5c56a103
HavQ <jonas.haverinen@helsinki.fi>
2021-07-19 00:56:32 +0300

    removed print test from monologue script

--- Scripts/UI/Monologue.gd
+++ Scripts/UI/Monologue.gd
@@ -7,11 +7,9 @@ func _ready():
 	self.set_bbcode("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 	self.set_visible_characters(0)
 	regex.compile("[A-Za-z]")
-	# $Timer.start()
 
 func _on_Timer_timeout():
 	self.set_visible_characters(self.get_visible_characters()+1)
-	print(get_bbcode())
 	
 	if self.get_visible_characters() >= self.get_total_character_count():
 		$Timer.stop()
