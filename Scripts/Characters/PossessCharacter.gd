156dd5b28010ad384462d1f9046c1d15b7ebb0d2
Ilmosal <ilmosal@gmail.com>
2021-07-20 23:41:16 +0300

    Fixed couple of bugs related to buttons and also ledge jumping now works properly

--- Scripts/Characters/PossessCharacter.gd
+++ Scripts/Characters/PossessCharacter.gd
@@ -264,8 +264,9 @@ func process_physics(delta):
 					x_speed -= sign(x_speed) * get_air_drag() * delta
 
 			if jump_time == 0.0 or (air_jump and jump):
-				y_speed = get_jump_start_speed() 
-				jump_time += delta
+				y_speed = get_jump_start_speed()
+				 
+				jump_time = delta
 				end_ledge_jump()
 
 			elif jump and jump_time < max_jump_time:
--- Scripts/Objects/Button.gd
+++ Scripts/Objects/Button.gd
@@ -1,19 +1,37 @@
 extends Node2D
 
-export(NodePath) var target
+export(Array, NodePath) var targets
 
 signal toggle_on
 signal toggle_off
 
+var button_on = false
+
+func get_weight_above():
+	var areas = []
+	
+	for a in $CheckWeight.get_overlapping_areas():
+		areas += a.get_parent().get_weight_above()
+
+	return areas
+	
 func _ready():
-	if target != null:
+	for target in targets:
 		connect("toggle_on", get_node(target), "toggle_on")
 		connect("toggle_off", get_node(target), "toggle_off")
 
 func _on_CheckWeight_area_entered(area):
+	if button_on:
+		return
+	
+	button_on = true
 	emit_signal("toggle_on")
 	$ButtonSprite.animation = "pressed"
 
 func _on_CheckWeight_area_exited(area):
+	if not get_weight_above().empty():
+		return
+	
+	button_on = false
 	emit_signal("toggle_off")
 	$ButtonSprite.animation = "default"
