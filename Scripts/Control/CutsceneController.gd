104280efa777bcbe1565b03a33bc7ef58f08f257
Ilmosal <ilmosal@gmail.com>
2021-07-21 15:25:15 +0300

    Added new placeholder maps to the game and added also saving

--- Scripts/Control/CutsceneController.gd
+++ Scripts/Control/CutsceneController.gd
@@ -1,9 +1,11 @@
 extends "res://Scripts/Control/Controllable.gd"
 
 signal proceed
+signal start_new_game
 
 func handle_inputs():
-	if Input.is_action_just_pressed("special") \
-	or Input.is_action_just_pressed("press_up") \
-	or Input.is_action_just_pressed("get_older"):
+	if Input.is_action_just_pressed("special"):
+		emit_signal("start_new_game")
+		
+	if Input.is_action_just_pressed("press_up") or Input.is_action_just_pressed("get_older"):
 		emit_signal("proceed")
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -8,6 +8,8 @@ enum MapEnum {
 	MAP_2_1, MAP_2_2, MAP_2_3, MAP_2_4, MAP_2_5, MAP_2_6, 
 	MAP_3_1, MAP_3_2, MAP_3_3, MAP_3_4, MAP_3_5, MAP_3_6, 
 	MAP_4_1, MAP_4_2, MAP_4_3, MAP_4_4, MAP_4_5, MAP_4_6, 
+	MAP_5_1, MAP_5_2, MAP_5_3, MAP_5_4, MAP_5_5, MAP_5_6, 
+	NO_SAVE, OVERRIDE_SAVE
 	}
 
 var final_map_dict = {
@@ -38,11 +40,55 @@ var final_map_dict = {
 	MapEnum.MAP_4_3: "res://Scenes/Maps/FinalMaps/Map4_3.tscn",
 	MapEnum.MAP_4_4: "res://Scenes/Maps/FinalMaps/Map4_4.tscn",
 	MapEnum.MAP_4_5: "res://Scenes/Maps/FinalMaps/Map4_5.tscn",
-	MapEnum.MAP_4_6: "res://Scenes/Maps/FinalMaps/Map4_6.tscn"
+	MapEnum.MAP_4_6: "res://Scenes/Maps/FinalMaps/Map4_6.tscn",
+	MapEnum.MAP_5_1: "res://Scenes/Maps/FinalMaps/Map5_1.tscn",
+	MapEnum.MAP_5_2: "res://Scenes/Maps/FinalMaps/Map5_2.tscn",
+	MapEnum.MAP_5_3: "res://Scenes/Maps/FinalMaps/Map5_3.tscn",
+	MapEnum.MAP_5_4: "res://Scenes/Maps/FinalMaps/Map5_4.tscn",
+	MapEnum.MAP_5_5: "res://Scenes/Maps/FinalMaps/Map5_5.tscn",
+	MapEnum.MAP_5_6: "res://Scenes/Maps/FinalMaps/Map5_6.tscn"
+}
+
+var map_music = {
+	MapEnum.TEST_ILMO: 0,
+	MapEnum.TEST_VILLE: 0,
+	MapEnum.TEST_JOHANNES: 0,
+	MapEnum.TEST_JOONAS: 0,
+	MapEnum.MAP_1_1: 0,
+	MapEnum.MAP_1_2: 0,
+	MapEnum.MAP_1_3: 0,
+	MapEnum.MAP_1_4: 0,
+	MapEnum.MAP_1_5: 0,
+	MapEnum.MAP_1_6: 0,
+	MapEnum.MAP_2_1: 1,
+	MapEnum.MAP_2_2: 1,
+	MapEnum.MAP_2_3: 1,
+	MapEnum.MAP_2_4: 1,
+	MapEnum.MAP_2_5: 1,
+	MapEnum.MAP_2_6: 1,
+	MapEnum.MAP_3_1: 2,
+	MapEnum.MAP_3_2: 2,
+	MapEnum.MAP_3_3: 2,
+	MapEnum.MAP_3_4: 2,
+	MapEnum.MAP_3_5: 2,
+	MapEnum.MAP_3_6: 2,
+	MapEnum.MAP_4_1: 3,
+	MapEnum.MAP_4_2: 3,
+	MapEnum.MAP_4_3: 3,
+	MapEnum.MAP_4_4: 3,
+	MapEnum.MAP_4_5: 3,
+	MapEnum.MAP_4_6: 3,
+	MapEnum.MAP_5_1: 3,
+	MapEnum.MAP_5_2: 3,
+	MapEnum.MAP_5_3: 3,
+	MapEnum.MAP_5_4: 3,
+	MapEnum.MAP_5_5: 3,
+	MapEnum.MAP_5_6: 3
 }
 
 export (MapEnum) var map = MapEnum.START_GAME
 export (MapEnum) var last_map = MapEnum.MAP_4_6
+export (MapEnum) var saved_map = MapEnum.NO_SAVE
 export var first_song = 10
 
 var current_gamestate = GameState.CONTROL_CUTSCENE
@@ -52,14 +98,19 @@ var current_map_resource = null
 var current_map = null
 var current_map_id = -1
 var normal_play = true
+var game_started = false
 
 func _ready():
 	$PauseController.control_id = GameState.CONTROL_PAUSE
 	$PlayerController.control_id = GameState.CONTROL_PLAYER
 	$CutsceneController.control_id = GameState.CONTROL_CUTSCENE
 
-	$CutsceneController.connect("proceed", $UIContainer,"proceed")
+	$CutsceneController.connect("proceed", self, "continue_game")
+	$CutsceneController.connect("start_new_game", self, "start_new_game")
 	$MusicManager.change_song(10)
+	
+	if saved_map == MapEnum.NO_SAVE:
+		load_game()
 	
 	if map != MapEnum.START_GAME:
 		$UIContainer.set_transparent()
@@ -72,6 +123,40 @@ func _ready():
 		current_map_id = MapEnum.MAP_1_1
 		$UIContainer.fade_in_start_screen()
 
+func start_new_game():
+	if not game_started:
+		save_game(MapEnum.MAP_1_1)
+		game_started = true
+	
+	$UIContainer.proceed()
+
+func continue_game():
+	if game_started:
+		$UIContainer.proceed()
+	else:
+		game_started = true
+		if saved_map == MapEnum.NO_SAVE:
+			start_new_game()
+		else:
+			$UIContainer.set_transparent()
+			current_map_id = saved_map
+			load_new_level()
+			start_new_level()
+			$MusicManager.change_song(map_music[current_map_id])
+			current_gamestate = GameState.CONTROL_PLAYER
+
+func save_game(map_id):
+	var save_file = File.new()
+	save_file.open("user://savegame.save", File.WRITE)
+	save_file.store_line(String(map_id))
+	save_file.close()
+
+func load_game():
+	var save_file = File.new()
+	save_file.open("user://savegame.save", File.READ)
+	saved_map = int(save_file.get_as_text())
+	save_file.close()
+
 func reset_map():
 	$UIContainer.fade_out_death()
 
@@ -79,13 +164,14 @@ func load_new_map():
 	$UIContainer.fade_straight_to_next_map()
 
 func start_first_song():
-	$MusicManager.change_song(first_song)
+	$MusicManager.change_song(map_music[current_map_id])
 
 func go_to_next_map():
 	$UIContainer.fade_out_level_title()
 	$MusicManager.change_active_character(-1)
 	$AudioEffectManager.play_sound("level_transition")
-	$MusicManager.change_song(current_map.map_music_id)
+	$MusicManager.change_song(map_music[current_map_id + 1])
+
 	if current_map_id == last_map:
 		next_gamestate = GameState.CONTROL_CUTSCENE
 		$UIContainer.fade_in_end_screen()
@@ -136,6 +222,7 @@ func load_new_level(reset = false):
 	$UIContainer.connect_character_to_ui(character_tmp)
 	$CanvasModulate.color = current_map.map_overlay_color
 	current_map.set_pause(true)
+	save_game(current_map_id)
 
 func start_new_level():
 	current_map.set_pause(false)
