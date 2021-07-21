682e79b4a40fd5202cb78e1f171514658fd122b7
Ilmosal <ilmosal@gmail.com>
2021-07-21 17:49:11 +0300

    Added blood splatters to the game

--- Scripts/Characters/PossessCharacter.gd
+++ Scripts/Characters/PossessCharacter.gd
@@ -28,11 +28,15 @@ var unloading_timer = 0.0
 var facing = 1.0
 var air_jump = false
 
+var blood_splatter_particles = null
+
 var character_state = CharacterState.IDLE
 var character_stage = CharacterStage.CHILLIN
 
 func _ready():
 	$JumpTimer.connect("timeout", self, "end_ledge_jump")
+	blood_splatter_particles = preload("res://Scenes/GameObjects/ParticleEffects/BloodSplatter.tscn").instance()
+	add_child(blood_splatter_particles)
 	$Light2D.max_alpha = 0.0
 
 func end_ledge_jump():
@@ -337,6 +341,7 @@ func process_physics(delta):
 
 func _on_HurtBox_area_entered(area):	
 	if area.get_collision_layer_bit(2):
+		blood_splatter_particles.emitting = true
 		character_state = CharacterState.DEATH
 	elif area.get_collision_mask_bit(8):
 		area.get_parent().start_destroy_projectile()
