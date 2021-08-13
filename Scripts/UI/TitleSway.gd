24e10993614d859b1a063d2cee37e5b499def61f
Ilmosal <ilmosal@gmail.com>
2021-08-13 21:36:56 +0300

    Added sway to game title and also tweaked some maps a little

---
+++ Scripts/UI/TitleSway.gd
@@ -0,0 +1,18 @@
+extends TextureRect
+
+
+var default_anchor_position = 0.0
+var current_sway = 0.0
+var sway_speed = 0.02
+var offset = 5.0
+
+func _ready():
+	default_anchor_position = rect_position.y
+
+func _process(delta):
+	current_sway += sway_speed
+	
+	rect_position.y = default_anchor_position + sin(current_sway) * offset
+	
+	if current_sway > 2*PI:
+		current_sway = 0.0
